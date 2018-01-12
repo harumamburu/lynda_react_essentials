4b6f17c7a6c1e5f9d115fac062c5c7fb6b929f76
harumamburu <nikolay.kovalev92@gmail.com>
2018-01-13 01:29:37 +0300

    Iinitial commit for Activity Counter

---
+++ ActivityCounter/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "./dist/assets"),
+        filename: "bundle.js",
+        publicPath: "assets"
+    },
+    devServer: {
+        inline: true,
+        contentBase: "./dist",
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["env", "react"]
+                }
+            }
+        ]
+    }
+};
